3c21e5c2d5e3e6ccae12f5a134ecefdf15dd7ede
nischalvasisth <nischalvasisth6@gmail.com>
2020-02-25 18:15:27 +0530

    giving final total

--- src/main/scala/com/knoldus/AkkaDispatcher.scala
+++ src/main/scala/com/knoldus/AkkaDispatcher.scala
@@ -2,14 +2,17 @@ package com.knoldus
 
   import java.io.File
 
+  import akka.pattern.ask
   import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
   import akka.routing.RoundRobinPool
   import akka.util.Timeout
   import com.typesafe.config.ConfigFactory
 
+  import scala.concurrent.ExecutionContext.Implicits.global
   import scala.concurrent.duration._
   import scala.io.Source
-  import com.knoldus.FileBasicOperation
+
+  import scala.concurrent.Future
 
 class AkkaDispatcher  extends  Actor with ActorLogging {
   def readAndOperate(filename: File): Map[String, Option[Int]] = {
@@ -23,7 +26,9 @@ class AkkaDispatcher  extends  Actor with ActorLogging {
  override def receive: Receive = {
     case msg: File =>
       val result = readAndOperate(msg)
-      log.info(self.path + result.toString())
+      //log.info(self.path + result.toString())
+        println(self.path+result.toString)
+        sender() ! result
     case _ => log.info("default case")
   }
 }
@@ -37,11 +42,18 @@ object AkkaDispatcher extends App  {
   val pathObj = new File("/home/knoldus/Downloads/Io2/")
   val list = pathObj.listFiles().toList
   val upadateFileList = list.filter(_.isFile)
-  upadateFileList.map(ll=>ref ! ll)
-  //implicit val timeout: Timeout = Timeout(4.seconds)
+  implicit val timeout: Timeout = Timeout(8.seconds)
+  val result = upadateFileList.map(ll=>ref ? ll)
+
+  val newResult=Future.sequence(result)
+  val finalResult = newResult.mapTo[List[Map[String, Option[Int]]]].map(_.foldLeft(Map("error"->0, "warn"->0, "info"->0)){ (result, countMap) => {
+    result++Map("error"->(result("error")+ countMap("error").getOrElse(0)))++Map("warn"->(result("warn")+ countMap("warn").getOrElse(0)))++Map("info"->(result("info")+ countMap("info").getOrElse(0)))
+  }})
+
+  finalResult.map(println)
 
 
- // system.terminate()
+  //system.terminate()
 }
 
 
